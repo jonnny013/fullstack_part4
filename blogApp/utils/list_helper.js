551bad16fdcd8f1a2f154334d12ef357d2b516a9
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-10-16 14:41:14 +0800

    Finish 4.5 favorite blog test

--- blogApp/utils/list_helper.js
+++ blogApp/utils/list_helper.js
@@ -8,11 +8,24 @@ const totalLikes = (total) => {
         const reducer = (sum, item) => {
             return sum + item
         }
-        return likes.reduce(reducer, 0)
+        return likes.reduce(reducer, 0)   
+}
+
+const favoriteBlog = (blogs) => {
+    const totallikes = blogs.map(a => a.likes)
+    if (totallikes.length === 0) {
+        return undefined
+    }
+    const max = Math.max(...totallikes)
     
+    const index = totallikes.indexOf(max)
+    const {title, author, likes} = blogs[index]
+    const result = {title, author, likes}
+    return result
 }
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes, 
+    favoriteBlog
 }
