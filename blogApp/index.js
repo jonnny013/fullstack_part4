273db3ae06f5ed6d6fccbb18ee84b572621a3d4c
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-10-16 12:08:18 +0800

    Set up backend structure

--- blogApp/index.js
+++ blogApp/index.js
@@ -1,1 +1,7 @@
-console.log('hi')
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+app.listen(config.PORT, () => {
+    logger.info(`Server is running on ${config.PORT}`)
+})
