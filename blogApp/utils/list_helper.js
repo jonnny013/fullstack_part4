ad9d093fe58a8ae4a03f7e7d2a7cdbc71e2eb8f4
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-10-16 13:59:45 +0800

    Add total like test

--- blogApp/utils/list_helper.js
+++ blogApp/utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (total) => {
+    const likes = total.map(a => a.likes)
+
+        const reducer = (sum, item) => {
+            return sum + item
+        }
+        return likes.reduce(reducer, 0)
+    
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
