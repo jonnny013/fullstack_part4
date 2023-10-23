f56ad9c0f3d90b5f467706604ce9343332027bd5
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-10-23 12:03:40 +0800

    Finish 4.21

--- blogApp/controllers/blogs.js
+++ blogApp/controllers/blogs.js
@@ -33,7 +33,18 @@ blogRouter.post('/', async (request, response) => {
 })
 
 blogRouter.delete('/:id', async (request, response) => {
-    await Blog.findByIdAndRemove(request.params.id)
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+    const blog = await Blog.findById(request.params.id)
+    if ( blog.user.toString() === user.id.toString()) {
+        await Blog.findByIdAndRemove(request.params.id)
+        console.log('deleted')
+    } else {
+        console.log('Not authorized to delete')
+    }
     response.status(204).end()
 })
 
